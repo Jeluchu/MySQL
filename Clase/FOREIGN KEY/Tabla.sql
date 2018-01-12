0f368d116ba47caab18333c4ff68435d4955411b
Jeluchu <jesusmarcalderon@gmail.com>
2018-01-12 08:58:22 +0100

    NEW UPDATE

---
+++ Clase/FOREIGN KEY/Tabla.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS Familia;
+CREATE DATABASE Familia;
+USE Familia;
+
+CREATE TABLE Padre (
+    ID INT NOT NULL,
+    PRIMARY KEY (ID)
+) ENGINE=INNODB;
+
+CREATE TABLE Hijo (
+    ID INT,
+    Padre_ID INT,
+    INDEX par_ind (padre_ID),
+    FOREIGN KEY (Padre_ID)
+        REFERENCES Padre(ID)
+        ON DELETE CASCADE
+) ENGINE=INNODB;
