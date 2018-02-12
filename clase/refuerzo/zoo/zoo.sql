b09627ed10dfe60dffa5ade7a8d316a93469794c
Jesús Mª <jeluchu@gmail.com>
2018-02-12 10:32:42 +0100

    FIX

--- clase/refuerzo/zoo/zoo.sql
+++ clase/refuerzo/zoo/zoo.sql
@@ -14,8 +14,8 @@ CREATE TABLE ZOO(
     nombre VARCHAR(20),
     ciudad VARCHAR(20),
     país VARCHAR(20),
-    tamaño enum('A','B'),
-    presupuesto float,
+    tamaño ENUM('A','B'),
+    presupuesto FLOAT UNSIGNED,
     PRIMARY KEY (nombre)
 );
 
@@ -31,12 +31,25 @@ CREATE TABLE ANIMAL(
     ID INT(4),
     nomzoo VARCHAR(20),
     nomespecie VARCHAR(20),
-    sexo enum('Masculino', 'Femenino'),
-    añonacim TIMESTAMP(4),
+    sexo ENUM('Masculino', 'Femenino'),
+    añonacim YEAR,
     país VARCHAR(20),
+    continente VARCHAR(20),
     PRIMARY KEY (ID),
-    FOREIGN KEY (nomzoo_ANIMAL)
-    REFERENCES ZOO(nombre),
-    FOREIGN KEY (nomespecie_ANIMAL)
-    REFERENCES ESPECIE(nombcientifico)
+    FOREIGN KEY (nomzoo)
+    REFERENCES ZOO (nombre),
+    FOREIGN KEY (nomespecie)
+    REFERENCES ESPECIE (nombcientifico)
 );
+
+/* PARTE B */
+ALTER TABLE ANIMAL MODIFY ID INT AUTO_INCREMENT;
+ALTER TABLE ANIMAL DROP FOREIGN KEY FK_ZOO;
+ALTER TABLE ANIMAL ADD CONSTRAINT FK_ZOO FOREIGN KEY (nomzoo)
+REFERENCES ZOO(nombre) ON DELETE SET NULL ON UPDATE CASCADE;
+
+/* PARTE C */
+
+INSERT INTO ZOO VALUES ('Zoo Madrid', 'Madrid', 'España', 'A', 608,42);
+INSERT INTO ESPECIE VALUES ('Panthera Tigris', 'Tigre', 'Felinos', 'Alto');
+INSERT INTO ANIMAL VALUES ('Zoo Madrid', 'Panthera Tigris', 'Macho', '2013', 'India', 'Asia');
